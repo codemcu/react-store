963e8a9ca492fd9666189ab5ec7075512a7d112f
Mauricio Correa <frontend.mcu@gmail.com>
2020-07-22 14:14:33 +0200

    add Main and Header components

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <header>Cool Sneakers Store</header>;
+};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main = () => {
+  return <main className="container">Main</main>;
+};
+
+export default Main;
