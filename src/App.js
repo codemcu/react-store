706c5f9a115c05f909bf8b53d1e62e2bd87aadb7
Mauricio Correa <frontend.mcu@gmail.com>
2020-07-22 14:14:01 +0200

    grid css, and media-queries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+
+import Header from './components/Header';
+import Main from './components/Main';
+
 import './App.scss';
 
 function App() {
-  return <div className="App">store</div>;
+  return (
+    <div className="app grid">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
