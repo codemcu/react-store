a18cf0d2f491455870aac27d40af642a8adff4c9
Mauricio Correa <frontend.mcu@gmail.com>
2020-07-23 20:12:21 +0200

    add context (useContext, useReducer)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 
 import Header from './components/Header';
 import Filter from './components/Filter';
-import Main from './components/Main';
+import SneakersList from './components/SneakersList';
+import ProductContext from './context/ProductContext';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="app grid">
-      <Header />
-      <div className="container">
-        <Filter />
-        <Main />
+    <ProductContext>
+      <div className="app grid">
+        <Header />
+        <div className="container">
+          <Filter />
+          <main>
+            <SneakersList />
+          </main>
+        </div>
       </div>
-    </div>
+    </ProductContext>
   );
 }
 
---
+++ src/context/ProductContext.js
@@ -0,0 +1,42 @@
+import React, { useReducer, createContext, useEffect } from 'react';
+import axios from 'axios';
+
+export const ProductContext = createContext();
+
+const initialState = [];
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'GET_PRODUCTS':
+      return {
+        ...state,
+        products: { ...action.products },
+      };
+    default:
+      return state;
+  }
+};
+
+const ProductContextProvider = (props) => {
+  const [products, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const { data } = await axios.get('/data/products.json');
+      dispatch({
+        type: 'GET_PRODUCTS',
+        products: data,
+      });
+    };
+
+    getProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products, dispatch }}>
+      {props.children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductContextProvider;
