45dc133b0bba5d2e415f40f36aa3b4a67a4d0843
Mauricio Correa <frontend.mcu@gmail.com>
2020-07-25 13:25:55 +0200

    implementation handleChange function to checkboxes

--- src/components/Sneaker.js
+++ src/components/Sneaker.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SneakerSizes from './SneakerSizes';
 import PropTypes from 'prop-types';
 
+import { ProductContext } from './../context/ProductContext';
+
 const Sneaker = ({ sneaker }) => {
+  const { state, dispatch } = useContext(ProductContext);
+
+  const handleChange = (sneaker) => {
+    dispatch({
+      type: 'SUM_PRODUCTS',
+      add: { ref: sneaker.ref, price: sneaker.price },
+    });
+  };
+
   return (
     <div className="sneakers-list-td">
       <div>
-        <input type="checkbox" />
+        <input type="checkbox" onChange={() => handleChange(sneaker)} />
       </div>
       <div className="sneakers-list-name">{sneaker.name}</div>
       <div>
