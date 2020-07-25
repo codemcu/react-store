1b4bb8cc7b18120b89009cba0d9a5511158fc933
Mauricio Correa <frontend.mcu@gmail.com>
2020-07-25 13:24:08 +0200

    add action.type to sum products

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -3,22 +3,47 @@ import axios from 'axios';
 
 export const ProductContext = createContext();
 
-const initialState = [];
+const initialState = {
+  products: [],
+  cart: [],
+};
 
 const reducer = (state = initialState, action) => {
+  const isCheched = (newProduct) => {
+    const founded = state.cart.filter(
+      (product) => product.ref === newProduct.ref
+    );
+    if (founded.length) {
+      const filtered = state.cart.filter(
+        (product) => product.ref !== newProduct.ref
+      );
+      return {
+        ...state,
+        cart: filtered,
+      };
+    } else {
+      return {
+        ...state,
+        cart: [...state.cart, newProduct],
+      };
+    }
+  };
+
   switch (action.type) {
     case 'GET_PRODUCTS':
       return {
         ...state,
         products: [...action.products],
       };
+    case 'SUM_PRODUCTS':
+      return isCheched(action.add);
     default:
       return state;
   }
 };
 
 const ProductContextProvider = (props) => {
-  const [products, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     const getProducts = async () => {
@@ -33,7 +58,7 @@ const ProductContextProvider = (props) => {
   }, []);
 
   return (
-    <ProductContext.Provider value={{ products, dispatch }}>
+    <ProductContext.Provider value={{ state, dispatch }}>
       {props.children}
     </ProductContext.Provider>
   );
